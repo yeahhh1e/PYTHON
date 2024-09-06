78adb34e1ef376c3f90dbd594edef106b4a4b13a
yeahhh1e <cakejuuu@gmail.com>
2024-09-06 17:49:24 +0900

    0906

---
+++ TEST/Weekly/0902/Algo1_서울_3반_황예원2.py
@@ -0,0 +1,38 @@
+import sys
+sys.stdin = open("algo1_sample_in.txt", "r")
+
+T = int(input())
+
+for tc in range(1, T+1):
+    N = int(input())
+    n_list = list(map(int, input()))
+
+    # print(N, n_list)
+
+    max_len = 0
+    for i in range(N-1):
+        if i == 0:  # 첫번째는 어차피 대칭구간 길이 안 되므로 검사 패스
+            pass
+        else:
+            center = i  # 중심 찾기
+            len_v = 0
+            for j in range(1, center+1): # 중심까지의 거리만큼 양 옆 탐색
+                if j == 1: # 양 옆 하나만 탐색할 때
+                    left = n_list[center-1]
+                    right = n_list[center+1]
+                else: # 두 개 탐색할 때
+                    left = n_list[center-1:center-j-1:-1] # 왼쪽으로 j만큼 담기
+                    # left = []
+                    # for x in range(len(left_list)-1, -1, -1):  # 마지막부터 첫 인덱스까지 거꾸로 순회하며 left에 담기
+                    #     left.append(left_list[x])
+                    # right = list(n_list[center+1: center+j+1])  # right는 중심값 다음부터 j까지 list로 담기
+                # else:   # 그 이상 탐색할 때
+                #     left = n_list[center-1:center-j-1:-1]
+                #     right = n_list[center+1:center+j+1]
+                if left == right:   # 왼쪽 오른쪽 비교
+                    len_v = len(n_list[center-j:center+j+1]) # 중심값 기준 양옆 j만큼의 길이 구하기
+                else: # 같지 않으면
+                    len_v = 1
+                if max_len < len_v:
+                    max_len = len_v
+    print(f"#{tc} {max_len}")
---
+++ TEST/Weekly/0902/Algo2_서울_3반_황예원.py
@@ -0,0 +1,17 @@
+import sys
+sys.stdin = open("algo2_sample_in.txt","r")
+
+T = int(input())
+for tc in range(1, T+1):
+    N = int(input())
+    string = str(input())
+    print_list = []
+    for s in string:  # 받은 문자열을 순회하며 아스키코드로 변환
+        asc = ord(s)
+        binary_num = bin(asc)   # 이진수로 변환
+        b_list = binary_num[2:] # 앞에 0b 떼주기
+
+        # 중위순회 하기
+        sort_list = b_list[3]+b_list[1]+b_list[4]+b_list[0]+b_list[5]+b_list[2]+b_list[6]
+        print_list.append(sort_list)
+    print(f"#{tc}", *print_list)
